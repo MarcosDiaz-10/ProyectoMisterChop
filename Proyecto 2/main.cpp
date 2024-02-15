3852527626ce1f4cfc59bacd5545d98756cf00f9
Marcos <tornadodiazelhalabi@gmail.com>
2024-02-15 06:43:16 -0400

    Listo la funcion de los menus

--- Proyecto 2/main.cpp
+++ Proyecto 2/main.cpp
@@ -6,15 +6,64 @@ void menuEquipos () {
 
     do
     {
-        cout << "1- Equipos" << endl;
-        cout << "2- Jugadores" << endl;
-        cout << "3- Directores Técnicos" << endl;
-        cout << "4- Partidos" << endl;
-        cout << "5- Salir" << endl;
+
+        cout << "1- Agregar" << endl;
+        cout << "2- Modificar" << endl;
+        cout << "3- Eliminar" << endl;
+        cout << "4- Listar Todos" << endl;
+        cout << "5- Volver" << endl;
 
         cin >> op;
 
-    } while ();
+    } while ( op != '5');
+    
+}
+void menuJugadores () {
+    char op;
+
+    do
+    {
+        cout << "1- Porteros" << endl;
+        cout << "2- Defensas" << endl;
+        cout << "3- Mediocampistas" << endl;
+        cout << "4- Delanteros" << endl;
+        cout << "5- Goleadores" << endl;
+        cout << "6- Volver" << endl;
+
+        cin >> op;
+
+    } while ( op != '6');
+    
+}
+void menuDirectorTecnico () {
+    char op;
+
+    do
+    {
+        cout << "1- Todos" << endl;
+        cout << "2- Los más experimentados" << endl;
+        cout << "3- Agregar" << endl;
+        cout << "4- Modificar" << endl;
+        cout << "5- Eliminar" << endl;
+        cout << "6- Volver" << endl;
+
+        cin >> op;
+
+    } while ( op != '6');
+    
+}
+
+void menuPartidos () {
+    char op;
+
+    do
+    {
+        cout << "1- Cargar Partidos" << endl;
+        cout << "2- Volver" << endl;
+
+        cin >> op;
+
+    } while ( op != '2');
     
 }
 
@@ -34,22 +83,26 @@ int main () {
         switch (op)
         {
         case '1':
-            
+            menuEquipos();
             break;
         case '2':
+            menuJugadores();
             break;
         case '3':
+            menuDirectorTecnico();
             break;
         case '4':
+            menuPartidos();
             break;
         case '5':
+            cout << "Cerrando el Programa...";
             break;
-        
         default:
+            cout << "Elija una opcion válida";
             break;
         }
 
-    } while (op != 5);
+    } while (op != '5');
     
 return 0;
 }
