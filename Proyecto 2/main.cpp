2decc71da9655d8e2b32eca3e85f2e5842087d1a
Marcos <tornadodiazelhalabi@gmail.com>
2024-02-15 20:50:41 -0400

    Listo la cuenta de entrenadores, jugadores y equipos de entrada.in

--- Proyecto 2/main.cpp
+++ Proyecto 2/main.cpp
@@ -1,5 +1,119 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+
+
+
+
+
+class Jugadores {
+    string nombre;
+    string apellido;
+    string posicion;
+    int experiencia;
+    string equipo;
+};
+
+class Equipos {
+    string nombre;
+    Jugadores *jugadores;
+};
+class DirectoresTecnicos {
+    string nombre;
+    string apellido;
+    int tiempoExperiencia;
+};
+
+class TrabajoArchivo {
+
+    public:
+    Equipos *equipos;
+    Jugadores *jugadores;
+    DirectoresTecnicos *directoresTecnicos;
+    int numEquipos = 0;
+    int numJugadores = 0;
+    int numDirectoresTecnicos = 0;
+    string nombreArchivo;
+
+    void contarArchivo () {
+        string line;
+        ifstream archivo(nombreArchivo.c_str());
+        int cont = 0;
+        char info;
+        if(archivo.is_open()) 
+        {
+            while (!archivo.eof())
+            {
+                string palabra;
+                
+                getline(archivo, line);
+
+                if(line == "E") {
+                    info = 'E';
+                    continue;
+                }
+
+                if( info == 'E') {
+                    numEquipos++;
+                }
+
+                if( line == "J" ) {
+                    info = 'J';
+                    continue;
+                }
+
+                if( info == 'J') {
+                    numJugadores++;
+                }
+                
+                if( line == "D" ) {
+                    info = 'D';
+                    continue;
+                }
+                
+                if( info == 'D') {
+                    numDirectoresTecnicos++;
+                }
+
+                cout << line << endl;
+
+
+            }
+            
+        }
+    }
+
+  
+    void leerEntrada(){
+        string line;
+        ifstream archivo(nombreArchivo.c_str());
+        if(archivo.is_open()) 
+        {
+            while (!archivo.eof())
+            {
+                string palabra;
+                
+                getline(archivo, line);
+
+                if(line == "E") {
+                    cout << "si entra en E";
+                    continue;
+                }
+
+                cout << line << endl;
+
+
+            }
+            
+        }
+    }
+
+    TrabajoArchivo( string nombreDeArchivo ) {
+        nombreArchivo = nombreDeArchivo;
+    }
+
+};
 
 void menuEquipos () {
     char op;
@@ -69,6 +183,16 @@ void menuPartidos () {
 
  
 int main () {
+
+    TrabajoArchivo entradaIn("Entrada.in.txt");
+
+    entradaIn.contarArchivo();
+
+    cout << entradaIn.numDirectoresTecnicos << endl;
+    cout << entradaIn.numEquipos << endl;
+    cout << entradaIn.numJugadores << endl;
+
+    
     char op;
     do
     {
